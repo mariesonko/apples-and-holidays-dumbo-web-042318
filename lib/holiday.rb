527015a340aa07068a8e9c71def52adec7df41e7
Marie Sonko <mariesonko62@gmail.com>
2018-04-19 22:46:28 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -15,6 +15,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
        :memorial_day => ["BBQ"]
      }
    }
+   holiday_hash[:summer][:fourth_of_july][1] 
   # return the second element in the 4th of July array
 end
 
@@ -22,7 +23,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-holiday_hash[:summer][:fourth_of_july][1]
+
 end
 
 
